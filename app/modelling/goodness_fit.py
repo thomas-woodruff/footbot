7323458680c24e77624dbb59c71b1d65646387c9
thomas-woodruff <thomas.woodruff@qubit.com>
2019-02-23 16:07:37 +0000

    fixing test

--- app/modelling/goodness_fit.py
+++ app/modelling/goodness_fit.py
@@ -1,6 +1,6 @@
 from scipy.stats import chi2
 
 def pearson_chi_squared_test(model, alpha=0.05):
-	reject_h0 = model.pearson_chi2 > chi2.ppf(0.95, model.df_resid)
-	p_value = chi2.cdf(0.95, model.df_resid)
+	reject_h0 = model.pearson_chi2 > chi2.ppf(1-alpha, model.df_resid)
+	p_value = chi2.cdf(model.pearson_chi2, model.df_resid)
 	return reject_h0, p_value
