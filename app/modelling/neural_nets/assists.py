6d09873f6c189a2380d86e206af206cc04026626
thomas-woodruff <thomas.woodruff@qubit.com>
2019-04-14 09:43:08 +0100

    neural nets

---
+++ app/modelling/neural_nets/assists.py
@@ -0,0 +1,28 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+
+
+def train_model(X, y):
+	scaler = StandardScaler().fit(X)
+	X = scaler.transform(X)
+
+	model = Sequential()
+	model.add(Dense(units=25, activation='relu', input_shape=(len(X.columns),)))
+	model.add(Dense(units=1, activation='linear'))
+
+	model.compile(
+	    loss='mean_absolute_error',
+	    optimizer='adam'
+	)
+
+	model.fit(X, y, epochs=20, batch_size=10, verbose=1)
+
+	return model
+
+
+def predict(X_pred):
+	scaler = StandardScaler().fit(X)
+	X = scaler.transform(X)
+
+	return model.predict(X_pred).flatten()
