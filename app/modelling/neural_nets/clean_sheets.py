b4f45a07546173c380553357dd100c2ccc1bfb3c
thomas-woodruff <thomas.woodruff@qubit.com>
2019-06-21 13:02:03 +0100

    cleaning neural net modules

--- app/modelling/neural_nets/clean_sheets.py
+++ app/modelling/neural_nets/clean_sheets.py
@@ -1,19 +1,15 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from sklearn.preprocessing import StandardScaler
 
 
-def train_model(X, y):
-	scaler = StandardScaler().fit(X)
-	X = scaler.transform(X)
-
+def train_model(X, y, loss='binary_crossentropy'):
 	model = Sequential()
 	model.add(Dense(units=25, activation='relu', input_shape=(len(X.columns),)))
 	model.add(Dense(units=1, activation='sigmoid'))
 
 	model.compile(
-	    loss='binary_crossentropy',
-	    optimizer='adam'
+		loss=loss,
+		optimizer='adam'
 	)
 
 	model.fit(X, y, epochs=20, batch_size=10, verbose=1)
@@ -22,7 +18,4 @@ def train_model(X, y):
 
 
 def predict(X_pred):
-	scaler = StandardScaler().fit(X)
-	X = scaler.transform(X)
-
 	return model.predict(X_pred).flatten()
