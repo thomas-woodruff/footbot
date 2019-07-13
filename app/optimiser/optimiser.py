4e647ff42851bb0462075c6627788da7106fc09c
thomas-woodruff <thomas.woodruff@qubit.com>
2019-07-13 12:10:44 +0100

    tinkering with optimiser

--- app/optimiser/optimiser.py
+++ app/optimiser/optimiser.py
@@ -33,28 +33,45 @@ def construct_optimal_team_from_scratch(
 	        else:
 	            player_team_weights[i, j] = 0
 
-	player_position_capacity = formation
-	bench_position_capacity = list(np.array([2, 5, 5, 3]) - np.array(player_position_capacity))
-
-	min_cost_keeper = np.min([i['value'] for i in players if i['element_type'] == 1])
-	min_cost_defender = np.min([i['value'] for i in players if i['element_type'] == 2])
-	min_cost_midfielder = np.min([i['value'] for i in players if i['element_type'] == 3])
-	min_cost_striker = np.min([i['value'] for i in players if i['element_type'] == 4])
-	min_cost_bench = \
-	bench_position_capacity@np.array([min_cost_keeper, min_cost_defender, min_cost_midfielder, min_cost_striker])
-
-
 	player_weights = np.concatenate((
 	    player_costs,
 	    player_position_weights,
 	    player_team_weights
 	), axis=0)
 	
-	player_num = 11
+	
 	bench_num = 4
+	bench_position_capacity = list(np.array([2, 5, 5, 3]) - np.array(player_position_capacity))
+	bench_team_capacity = [3]*20
 
-	bench_team_capacity = [3]*20 #list(player_team_capacity - player_team_weights@player_selection)	
-	bench_cost_capacity = [min_cost_bench + 50] #[total_budget - player_selection_cost]
+
+
+
+	bench_cost_x = cp.Variable(len(players), boolean=True)
+
+	bench_cost_prob = cp.Problem(
+	    cp.Minimize(player_costs@bench_x),
+	    [
+	        np.concatenate((player_position_weights,player_team_weights), axis=0)@bench_x <= np.array(bench_position_capacity + bench_team_capacity),
+	        np.ones(len(players))@bench_x == bench_num
+	    ]
+	)
+
+	min_cost_bench = bench_cost_prob.solve()
+
+
+
+
+
+	# min_cost_keeper = np.min([i['value'] for i in players if i['element_type'] == 1])
+	# min_cost_defender = np.min([i['value'] for i in players if i['element_type'] == 2])
+	# min_cost_midfielder = np.min([i['value'] for i in players if i['element_type'] == 3])
+	# min_cost_striker = np.min([i['value'] for i in players if i['element_type'] == 4])
+	# min_cost_bench = \
+	# bench_position_capacity@np.array([min_cost_keeper, min_cost_defender, min_cost_midfielder, min_cost_striker])
+
+	
+	bench_cost_capacity = [min_cost_bench]
 
 	bench_capacity = np.array(
 	    bench_cost_capacity
@@ -68,19 +85,18 @@ def construct_optimal_team_from_scratch(
 	    cp.Maximize(player_points@bench_x),
 	    [
 	        player_weights@bench_x <= bench_capacity,
-	        np.ones(len(players))@bench_x == bench_num,
-	        #np.array(player_selection)@bench_x <= 0.01
+	        np.ones(len(players))@bench_x == bench_num
 	    ]
 	)
 
-	print(bench_prob.solve())
-	# print('bench_prob.status', bench_prob.status)
+	bench_prob.solve()
 	bench_selection = [int(round(j)) for j in bench_x.value]
 	bench_selection_indices = [i for i, j in enumerate(bench_selection) if j == 1]
 	bench_selection_elements = player_elements[bench_selection_indices]
-	print('bench_selection_elements', bench_selection_elements)
 
 
+	player_num = 11
+	player_position_capacity = formation
 	player_cost_capacity = [total_budget - min_cost_bench]
 	player_team_capacity = list(bench_team_capacity - player_team_weights@bench_selection) #[3]*20
 
@@ -101,8 +117,7 @@ def construct_optimal_team_from_scratch(
 	    ]
 	)
 
-	print(player_prob.solve())
-	# print('player_prob.status', player_prob.status)
+	player_prob.solve()
 	player_selection = [int(round(j)) for j in player_x.value]
 	player_selection_indices = [i for i, j in enumerate(player_selection) if j == 1]
 	player_selection_elements = player_elements[player_selection_indices]
