a789892800f71ed83dc3b9535f0260bee8c64130
thomas-woodruff <thomas.woodruff@qubit.com>
2019-09-08 04:38:02 +0100

    element data

--- app/data/element_data.py
+++ app/data/element_data.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import requests
 import datetime
+from .utils import get_safe_web_name
 
 
 def get_element_df():
+    '''get contemporaneous element data'''
     bootstrap_request = requests.get('https://fantasy.premierleague.com/api/bootstrap-static/')
     bootstrap_data = bootstrap_request.json()
 
@@ -11,15 +13,15 @@ def get_element_df():
 
     current_datetime = str(datetime.datetime.now())
 
-
     element_df = pd.DataFrame(bootstrap_data['elements'])
 
     element_df['element'] = element_df['id']
     element_df['current_event'] = current_event
     element_df['datetime'] = current_datetime
+    element_df['safe_web_name'] = get_safe_web_name(element_df['web_name'])
 
     element_df = element_df[[
-        'element', 'current_event', 'datetime', 'web_name', 'assists', 'bonus', 'bps',
+        'element', 'current_event', 'datetime', 'safe_web_name', 'assists', 'bonus', 'bps',
         'chance_of_playing_next_round', 'chance_of_playing_this_round',
         'clean_sheets', 'code', 'cost_change_event', 'cost_change_event_fall',
         'cost_change_start', 'cost_change_start_fall', 'creativity',
@@ -31,7 +33,7 @@ def get_element_df():
         'selected_by_percent', 'special', 'squad_number', 'status', 'team',
         'team_code', 'threat', 'total_points', 'transfers_in',
         'transfers_in_event', 'transfers_out', 'transfers_out_event', 'value_form',
-        'value_season', 'yellow_cards'
+        'value_season', 'web_name', 'yellow_cards'
     ]]
 
     return element_df
