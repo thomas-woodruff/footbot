5c77a706e924882287c143c63b02f18c9e53a412
thomas-woodruff <thomas.woodruff@qubit.com>
2019-09-21 16:41:17 +0100

    gclouding

--- footbot/main.py
+++ footbot/main.py
@@ -1,20 +1,32 @@
 import logging
 import schedule
 import time
+import multiprocessing as mp
 from footbot.data import element_data, entry_data, utils
+from footbot.api import server
+
+log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=log_fmt)
 
 
 def element_data_job():
+    logger = logging.getLogger(__name__)
+
+    logger.info('getting element data')
     element_df = element_data.get_element_df()
+    logger.info('writing element data')
     utils.write_to_table('fpl',
                          'element_data_1920',
                          element_df)
 
+    logger.info('getting element gameweeks and fixtures')
     element_history_df, element_fixtures_df = element_data.get_element_summary_dfs()
+    logger.info('writing element gameweeks')
     utils.write_to_table('fpl',
                          'element_gameweeks_1920',
                          element_history_df,
                          write_disposition='WRITE_TRUNCATE')
+    logger.info('writing element fixtures')
     utils.write_to_table('fpl',
                          'element_future_fixtures_1920',
                          element_fixtures_df,
@@ -22,29 +34,41 @@ def element_data_job():
 
 
 def entry_data_job():
+    logger = logging.getLogger(__name__)
+
+    logger.info('getting entry data')
     picks_df, chips_df = entry_data.get_top_entries_dfs()
+    logger.info('writing entry picks')
     utils.write_to_table('fpl',
                          'top_entries_picks_1920',
                          picks_df,
                          write_disposition='WRITE_TRUNCATE')
+    logger.info('writing entry chips')
     utils.write_to_table('fpl',
                          'top_entries_chips_1920',
                          chips_df,
                          write_disposition='WRITE_TRUNCATE')
 
 
-def main():
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-    logger = logging.getLogger(__name__)
-
+def scheduled_processes():
     schedule.every().day.at('09:30').do(element_data_job)
     schedule.every().day.at('09:35').do(entry_data_job)
 
-    logger.info('starting schedule')
     while True:
         schedule.run_pending()
         time.sleep(1)
+
+
+def main():
+    logger = logging.getLogger(__name__)
+
+    p = mp.Process(target=scheduled_processes)
+
+    logger.info('starting scheduler')
+    p.start()
+
+    logger.info('starting api')
+    server.run()
 
 
 if __name__ == '__main__':
