97c7c75439b9f10e4fce7f95534bd4c37ff7e343
thomas-woodruff <thomas.woodruff@qubit.com>
2020-02-02 12:24:27 +0000

    all elements to be updated. do entries too

--- footbot/data/entry_data.py
+++ footbot/data/entry_data.py
@@ -11,25 +11,22 @@ def get_top_entries():
     return list(client.query(sql).to_dataframe()['entry'].values)
 
 
-def get_top_entries_dfs():
-    '''get player picks and chip usage of top entries'''
-    entry_season_arr = []
-
+def get_top_entry_dfs(entry):
     bootstrap_request = requests.get('https://fantasy.premierleague.com/api/bootstrap-static/')
     bootstrap_data = bootstrap_request.json()
 
     current_event = [i for i in bootstrap_data['events'] if i['is_current']][0]['id']
 
-    for i in get_top_entries():
-        for j in range(1, current_event + 1):
-            try:
-                entry_season_request = requests.get(f'https://fantasy.premierleague.com/api/entry/{i}/event/{j}/picks/')
-                entry_season_data = entry_season_request.json()
-                entry_season_data['entry'] = i
-                entry_season_data['event'] = j
-                entry_season_arr.append(entry_season_data)
-            except:
-                continue
+    entry_season_arr = []
+    for event in range(1, current_event + 1):
+        try:
+            entry_season_request = requests.get(f'https://fantasy.premierleague.com/api/entry/{i}/event/{j}/picks/')
+            entry_season_data = entry_season_request.json()
+            entry_season_data['entry'] = entry
+            entry_season_data['event'] = event
+            entry_season_arr.append(entry_season_data)
+        except:
+            continue
 
     picks_df = pd.DataFrame(
         [
--- footbot/main.py
+++ footbot/main.py
@@ -30,6 +30,24 @@ def create_update_element_history_fixtures_task(element):
     return response
 
 
+def create_update_entry_picks_chips_task(entry):
+    logger = logging.getLogger(__name__)
+    logger.info(f'doing entry {entry}')
+
+    task = {
+        'app_engine_http_request': {
+            'http_method': 'POST',
+            'relative_uri': f'/update_entry_picks_chips/{entry}'
+        }
+    }
+
+    response = utils.create_cloud_task(
+        task,
+        'update-entry-picks-chips')
+
+    return response
+
+
 def update_element_history_fixtures_worker(element):
     logger = logging.getLogger(__name__)
 
@@ -55,6 +73,33 @@ def update_element_history_fixtures_worker(element):
                          'element_future_fixtures_1920',
                          element_fixtures_df)
     logger.info('done writing element fixtures')
+
+
+def update_entry_picks_chips_worker(entry):
+    logger = logging.getLogger(__name__)
+
+    logger.info('getting entry data')
+    picks_df, chips_df = entry_data.get_top_entry_dfs(entry)
+
+    logger.info('deleting entry picks')
+    utils.run_query(
+        f'DELETE FROM `footbot-001.fpl.top_entries_picks_1920` WHERE entry = {entry}'
+    )
+    logger.info('writing entry picks')
+    utils.write_to_table('fpl',
+                         'top_entries_picks_1920',
+                         picks_df)
+    logger.info('done writing entry picks')
+
+    logger.info('deleting entry chips')
+    utils.run_query(
+        f'DELETE FROM `footbot-001.fpl.top_entries_chips_1920` WHERE entry = {entry}'
+    )
+    logger.info('writing entry chips')
+    utils.write_to_table('fpl',
+                         'top_entries_chips_1920',
+                         chips_df,)
+    logger.info('done writing entry chips')
 
 
 @app.route('/')
@@ -87,7 +132,7 @@ def update_element_history_fixtures_route():
     elements = element_data.get_elements()
 
     with ThreadPoolExecutor(max_workers=50) as executor:
-        executor.map(create_update_element_history_fixtures_task, elements[:10])
+        executor.map(create_update_element_history_fixtures_task, elements)
 
     return 'elements queued'
 
@@ -103,28 +148,30 @@ def update_element_history_fixtures_element_route(element):
         return 'bad news!'
 
 
+@app.route('/update_entry_picks_chips')
+def update_entry_picks_chips_route():
+    logger = logging.getLogger(__name__)
 
-# this takes at least a few minutes to run at the moment
-# @app.route('/update_entry_picks_chips')
-# def update_entry_picks_chips():
-#     logger = logging.getLogger(__name__)
-#
-#     logger.info('getting entry data')
-#     picks_df, chips_df = entry_data.get_top_entries_dfs()
-#     logger.info('writing entry picks')
-#     utils.write_to_table('fpl',
-#                          'top_entries_picks_1920',
-#                          picks_df,
-#                          write_disposition='WRITE_TRUNCATE')
-#     logger.info('done writing entry picks')
-#     logger.info('writing entry chips')
-#     utils.write_to_table('fpl',
-#                          'top_entries_chips_1920',
-#                          chips_df,
-#                          write_disposition='WRITE_TRUNCATE')
-#     logger.info('done writing entry chips')
-#
-#     return 'Updated entry picks and chips, baby'
+    logger.info('purging queue')
+    utils.purge_cloud_queue('update-entry-picks-chips')
+
+    entries = entry_data.get_top_entries()
+
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        executor.map(create_update_entry_picks_chips_task, entries)
+
+    return 'entries queued'
+
+
+@app.route('/update_entry_picks_chips/<entry>', methods=['POST'])
+def update_entry_picks_chips_route(entry):
+    logger = logging.getLogger(__name__)
+    try:
+        update_entry_picks_chips_worker(entry)
+        return 'lovely stuff'
+    except Exception as e:
+        logger.error(f'Unable to update entry {entry} with exception {e}')
+        return 'bad news!'
 
 
 @app.route('/optimise_team/<entry>')
