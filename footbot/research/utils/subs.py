dc65ee1a25caa79fcbe462a91a73f955cab504ac
thomas-woodruff <thomas.woodruff@qubit.com>
2020-11-06 15:55:56 +0000

    captain picks and formatting

--- footbot/research/utils/subs.py
+++ footbot/research/utils/subs.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 
 def check_valid_formation(first_team):
-    '''
+    """
     Check candidate first team has a valid formation.
 
     https://fantasy.premierleague.com/help
@@ -10,9 +10,9 @@ def check_valid_formation(first_team):
 
     :param first_team: Array of dicts representing first team
     :return: Boolean indicating validity
-    '''
+    """
 
-    c = Counter(i['element_type'] for i in first_team)
+    c = Counter(i["element_type"] for i in first_team)
 
     keepers = c[1] == 1
     defenders = c[2] >= 3
@@ -22,20 +22,22 @@ def check_valid_formation(first_team):
 
 
 def make_subs(first_team, bench):
-    '''
+    """
     Substitute missing players in the first team for players on the bench, ensuring valid formation.
 
     :param first_team: Array of dicts representing first team
     :param bench: Array of dicts representing bench
     :return: Tuple of updated first_team and bench
-    '''
+    """
 
     squad = first_team + bench
-    missing_players = [i for i in first_team if i['minutes'] == 0]
+    missing_players = [i for i in first_team if i["minutes"] == 0]
 
     for b in bench:
         for m in missing_players:
-            candidate_first_team = [i for i in first_team if i['element'] != m['element']] + [b]
+            candidate_first_team = [
+                i for i in first_team if i["element"] != m["element"]
+            ] + [b]
 
             if check_valid_formation(candidate_first_team):
                 first_team = candidate_first_team
@@ -47,6 +49,27 @@ def make_subs(first_team, bench):
     return first_team, bench
 
 
-def get_captain(captain, vice):
-    return "bar"
+def pick_captain(first_team):
+    """
+    Change the captain and vice if necessary.
 
+    If the captain doesn't play, the vice becomes captain.
+    If both vice and captain don't play, there is no captain.
+
+    :param first_team: Array of dicts representing first team
+    :return: Array of dicts representing updated first team
+    """
+    captain_minutes = [i["minutes"] for i in first_team if i["is_captain"]][0]
+    vice_minutes = [i["minutes"] for i in first_team if i["is_vice"]][0]
+
+    if captain_minutes == 0:
+        for player in first_team:
+            if player["is_captain"]:
+                player["is_captain"] = False
+        for player in first_team:
+            if player["is_vice"]:
+                player["is_vice"] = False
+                if vice_minutes > 0:
+                    player["is_captain"] = True
+
+    return first_team
--- tests/unit/research/utils/test_subs.py
+++ tests/unit/research/utils/test_subs.py
@@ -1,31 +1,124 @@
 import pytest
-from footbot.research.utils.subs import check_valid_formation, make_subs
+
+from footbot.research.utils.subs import check_valid_formation
+from footbot.research.utils.subs import make_subs
+from footbot.research.utils.subs import pick_captain
 
 
 @pytest.fixture()
 def first_team():
     return [
-        {"element": 1, "element_type": 1, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 2, "element_type": 2, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 3, "element_type": 2, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 4, "element_type": 2, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 5, "element_type": 3, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 6, "element_type": 3, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 7, "element_type": 3, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 8, "element_type": 3, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 9, "element_type": 4, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 10, "element_type": 4, "minutes": 90, "is_captain": 0, "is_vice": 1},
-        {"element": 11, "element_type": 4, "minutes": 90, "is_captain": 1, "is_vice": 0},
+        {
+            "element": 1,
+            "element_type": 1,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 2,
+            "element_type": 2,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 3,
+            "element_type": 2,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 4,
+            "element_type": 2,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 5,
+            "element_type": 3,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 6,
+            "element_type": 3,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 7,
+            "element_type": 3,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 8,
+            "element_type": 3,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 9,
+            "element_type": 4,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 10,
+            "element_type": 4,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": True,
+        },
+        {
+            "element": 11,
+            "element_type": 4,
+            "minutes": 90,
+            "is_captain": True,
+            "is_vice": False,
+        },
     ]
 
 
 @pytest.fixture()
 def bench():
     return [
-        {"element": 12, "element_type": 1, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 15, "element_type": 3, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 13, "element_type": 2, "minutes": 90, "is_captain": 0, "is_vice": 0},
-        {"element": 14, "element_type": 2, "minutes": 90, "is_captain": 0, "is_vice": 0},
+        {
+            "element": 12,
+            "element_type": 1,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 15,
+            "element_type": 3,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 13,
+            "element_type": 2,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
+        {
+            "element": 14,
+            "element_type": 2,
+            "minutes": 90,
+            "is_captain": False,
+            "is_vice": False,
+        },
     ]
 
 
@@ -44,11 +137,11 @@ def test_check_valid_formation_invalid(first_team):
 
 def test_make_subs_no_subs(first_team, bench):
     ft, b = make_subs(first_team, bench)
-    ft = [i['element'] for i in ft]
-    b = [i['element'] for i in b]
+    ft = [i["element"] for i in ft]
+    b = [i["element"] for i in b]
 
-    expected_ft = [i['element'] for i in first_team]
-    expected_b = [i['element'] for i in bench]
+    expected_ft = [i["element"] for i in first_team]
+    expected_b = [i["element"] for i in bench]
 
     assert set(ft) == set(expected_ft)
     assert set(b) == set(expected_b)
@@ -60,8 +153,8 @@ def test_make_subs_all_subs(first_team, bench):
             player["minutes"] = 0
 
     ft, b = make_subs(first_team, bench)
-    ft = [i['element'] for i in ft]
-    b = [i['element'] for i in b]
+    ft = [i["element"] for i in ft]
+    b = [i["element"] for i in b]
 
     ft_expected = [12, 2, 3, 4, 5, 6, 7, 15, 9, 10, 13]
     b_expected = [1, 8, 11, 14]
@@ -76,11 +169,46 @@ def test_make_subs_some_subs(first_team, bench):
             player["minutes"] = 0
 
     ft, b = make_subs(first_team, bench)
-    ft = [i['element'] for i in ft]
-    b = [i['element'] for i in b]
+    ft = [i["element"] for i in ft]
+    b = [i["element"] for i in b]
 
     ft_expected = [1, 13, 14, 4, 5, 6, 7, 8, 9, 10, 11]
     b_expected = [12, 2, 3, 15]
 
     assert set(ft) == set(ft_expected)
     assert set(b) == set(b_expected)
+
+
+def test_get_captain_no_change(first_team):
+    ft = pick_captain(first_team)
+    c = [i["element"] for i in ft if i["is_captain"]]
+    v = [i["element"] for i in ft if i["is_vice"]]
+
+    assert c == [11]
+    assert v == [10]
+
+
+def test_get_captain_sub(first_team):
+    for player in first_team:
+        if player["is_captain"]:
+            player["minutes"] = 0
+
+    ft = pick_captain(first_team)
+    c = [i["element"] for i in ft if i["is_captain"]]
+    v = [i["element"] for i in ft if i["is_vice"]]
+
+    assert c == [10]
+    assert v == []
+
+
+def test_get_captain_both_missing(first_team):
+    for player in first_team:
+        if player["is_captain"] or player["is_vice"]:
+            player["minutes"] = 0
+
+    ft = pick_captain(first_team)
+    c = [i["element"] for i in ft if i["is_captain"]]
+    v = [i["element"] for i in ft if i["is_vice"]]
+
+    assert c == []
+    assert v == []
