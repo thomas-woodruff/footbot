ac4ae338a609675ac61116a941ab0126b28dadb0
thomas-woodruff <thomas.woodruff@qubit.com>
2021-01-18 10:53:59 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="footbot",
-    version="0.1.0",
+    version="0.2.0",
     url='https://github.com/thomas-woodruff/footbot',
     author='thomas-woodruff',
     license='All rights reserved.',
